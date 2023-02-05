86812021394c91e627eaf5009fdb10c13e578127
unohayato <mamidori1p1p@gmail.com>
2023-02-05 22:03:23 +0900

    feat: :sparkeles: add formatter

--- logging/log.py
+++ logging/log.py
@@ -1,10 +1,12 @@
-from logging import getLogger, StreamHandler, DEBUG
+from logging import getLogger, StreamHandler, Formatter, DEBUG, ERROR, INFO
 
+formatter = Formatter('[%(levelname)s] %(asctime)s - %(message)s (%(filename)s)')
 logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
-logger.setLevel(DEBUG)
+handler.setFormatter(formatter)
+logger.setLevel(INFO)
 logger.addHandler(handler)
-logger.debug('入力値はxxです')
+logger.info('入力値はxxです')
 
 
