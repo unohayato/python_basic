80276fff7fbde67af4156f6f86b528037ff5827b
unohayato <mamidori1p1p@gmail.com>
2023-02-05 22:15:20 +0900

    feat: :sparkles: add fileHandler

--- logging/log.py
+++ logging/log.py
@@ -1,12 +1,24 @@
-from logging import getLogger, StreamHandler, Formatter, DEBUG, ERROR, INFO
+from logging import getLogger, StreamHandler, FileHandler,Formatter, DEBUG, ERROR, INFO
 
 formatter = Formatter('[%(levelname)s] %(asctime)s - %(message)s (%(filename)s)')
 logger = getLogger(__name__)
-handler = StreamHandler()
+
+handler = FileHandler('log2.txt')
 handler.setLevel(DEBUG)
 handler.setFormatter(formatter)
-logger.setLevel(INFO)
+
+error_handler = FileHandler('error.txt')
+error_handler.setLevel(ERROR)
+error_handler.setFormatter(formatter)
+
+logger.setLevel(DEBUG)
 logger.addHandler(handler)
-logger.info('入力値はxxです')
+logger.addHandler(error_handler)
+
+
+logger.debug('入力値は1000です')
+logger.info('プログラムが開始しました')
+logger.warning('ファイルの容量が299GBを越えました')
+logger.error('ファイルが存在していません')
 
 
