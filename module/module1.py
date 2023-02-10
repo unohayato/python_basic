80135e5157a05be8034675ffec2dbdbeb87b4222
unohayato <mamidori1p1p@gmail.com>
2023-02-10 21:08:42 +0900

    feat: :sparkles: add modules

---
+++ module/module1.py
@@ -0,0 +1,7 @@
+import module2
+
+def main():
+  module2.func2()
+
+if __name__ == '__main__':
+  main()
---
+++ module/module2.py
@@ -0,0 +1,8 @@
+def func2():
+  print('func2です。')
+
+def func3():
+  print('func3です。')
+
+if __name__ == '__main__':
+  func3()
