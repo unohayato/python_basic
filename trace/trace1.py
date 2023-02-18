06d006da741e911dbabc66c9260d13ed4afe8213
unohayato <mamidori1p1p@gmail.com>
2023-02-18 21:22:02 +0900

    feat: :sparkles: add how to read traceback

--- trace/trace1.py
+++ trace/trace1.py
@@ -1,12 +1,18 @@
 import traceback
+import trace2
 
-try:
-  result = 1 / 0
-  print(result)
-except ZeroDivisionError:
-  print('---トレースバックを開始---')
-  t = traceback.format_exc()
-  print(t)
-  print('---トレースバックを終了---')
+def main():
+  trace2.func1()
   
+if __name__ == '__main__':
+  main()
   
+
+# try:
+#   result = 1 / 0
+#   print(result)
+# except ZeroDivisionError:
+#   print('---トレースバックを開始---')
+#   t = traceback.format_exc()
+#   print(t)
+#   print('---トレースバックを終了---')
---
+++ trace/trace2.py
@@ -0,0 +1,6 @@
+def func1():
+  func2()
+  
+def func2():
+  result = 1 / 0
+  print(result)
