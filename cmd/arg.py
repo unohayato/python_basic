af82c9e1bb3151b0f66a105a667856a75ff3a541
unohayato <mamidori1p1p@gmail.com>
2023-02-21 21:54:51 +0900

    feat: :fire: add language

--- cmd/arg.py
+++ cmd/arg.py
@@ -1,12 +1,19 @@
 import argparse
 
 if __name__ == '__main__':
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument(
+  #   'numbers', 
+  #   help='調べたい数値を入力してください。',
+  #   type=int,
+  #   nargs=3
+  #   )
+  # args = parser.parse_args()
+  # print(args.numbers)
   parser = argparse.ArgumentParser()
   parser.add_argument(
-    'numbers', 
-    help='調べたい数値を入力してください。',
-    type=int,
-    nargs=3
-    )
+    'language',
+    choices=['English', 'Japanese']
+  )
   args = parser.parse_args()
-  print(args.numbers)
+  print(args.language)
