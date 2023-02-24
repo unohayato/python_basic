0978db4d1753f69c9c6df7c8eb0e7be3b0b6e85c
unohayato <mamidori1p1p@gmail.com>
2023-02-24 20:55:45 +0900

    feat: :fire: add iterdir

--- path/path.py
+++ path/path.py
@@ -1,4 +1,10 @@
 from pathlib import Path
 
 path = Path.cwd()
-dir1 = path / 'dir1'
+print(path)
+dir1 = path / 'dir1' / 'dir2'
+print(dir1)
+
+dir1_g = dir1.iterdir()
+for x in dir1_g:
+  print(x.name)
