cd2248b53d40750a0ec89d9f5adb8ef0c50b355b
unohayato <mamidori1p1p@gmail.com>
2023-02-26 18:49:55 +0900

    feat: :fire: add paths

--- path/path.py
+++ path/path.py
@@ -6,5 +6,10 @@ dir1 = path / 'dir1' / 'dir2'
 print(dir1)
 
 dir1_g = dir1.iterdir()
-for x in dir1_g:
-  print(x.name)
+
+
+# for x in dir1_g:
+#   print(x.name)
+
+paths =  [str(x) for x in dir1_g]
+print(paths)
