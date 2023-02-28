ebcb3934d6aed1e76569376d708ef96950ed2818
unohayato <mamidori1p1p@gmail.com>
2023-02-28 23:41:28 +0900

    feat: :fire: add nan

--- nan_inf/nan_inf.py
+++ nan_inf/nan_inf.py
@@ -1,6 +1,12 @@
 import sys
-max_num = sys.float_info.max
-print(max_num)
-print(max_num * 2)
+# max_num = sys.float_info.max
+# print(max_num)
+# print(max_num * 2)
 
-print(float('inf'))
+# print(float('inf'))
+
+import math
+
+x = float('inf') - float('inf')
+print(x)
+print(math.isnan(x))
